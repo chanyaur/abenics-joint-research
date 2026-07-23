ad274eaa4a2b552881bd47561d3d0022c1fd2db7
Chanya Methaprayoon <chanya.meth@gmail.com>
2026-07-23 08:06:19 -0700

    working (i think?) for a larger sine

--- REAL MPC Controller/test_animation2.m
+++ REAL MPC Controller/test_animation2.m
@@ -1,10 +1,12 @@
+%% run this file first before running reduced_abenics.slx
+
 time = (0:params.Ts:10)';
 
 q = repmat(deg2rad([15 10 5]),length(time),1);
 
-q(:,2) = deg2rad(10) + deg2rad(5)*sin(2*pi*0.2*time);  % 5 deg to 10 deg
+% q(:,2) = deg2rad(10) + deg2rad(5)*sin(2*pi*0.2*time);  % 5 deg to 10 deg
 
-% q(:,2) = deg2rad(10) + deg2rad(30)*sin(2*pi*0.1*time);  % -20 deg to 40 deg
+q(:,2) = deg2rad(10) + deg2rad(30)*sin(2*pi*0.1*time);  % -20 deg to 40 deg
 
 q_ref_ts = timeseries(q,time);
 
