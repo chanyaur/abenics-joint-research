55963b729b6b3c7356ce86d949c8e5b650a48732
Chanya Methaprayoon <chanya.meth@gmail.com>
2026-07-23 07:51:35 -0700

    working for very small sine wave

---
+++ REAL MPC Controller/test_animation2.m
@@ -0,0 +1,11 @@
+time = (0:params.Ts:10)';
+
+q = repmat(deg2rad([15 10 5]),length(time),1);
+
+q(:,2) = deg2rad(10) + deg2rad(5)*sin(2*pi*0.2*time);  % 5 deg to 10 deg
+
+% q(:,2) = deg2rad(10) + deg2rad(30)*sin(2*pi*0.1*time);  % -20 deg to 40 deg
+
+q_ref_ts = timeseries(q,time);
+
+disp("loaded animation constant to feed into MPC");
