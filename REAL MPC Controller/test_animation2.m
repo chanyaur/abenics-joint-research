1a3566b67adf118fb37bfad95c350ca938891dfe
Chanya Methaprayoon <chanya.meth@gmail.com>
2026-07-23 20:23:31 -0700

    a thing

--- REAL MPC Controller/test_animation2.m
+++ REAL MPC Controller/test_animation2.m
@@ -1,12 +1,26 @@
-%% run this file first before running reduced_abenics.slx
+% %% run this file first before running reduced_abenics.slx
+% 
+% time = (0:params.Ts:10)';
+% 
+% q = repmat(deg2rad([15 10 5]),length(time),1);
+% 
+% % q(:,2) = deg2rad(10) + deg2rad(5)*sin(2*pi*0.2*time);  % 5 deg to 10 deg
+% 
+% q(:,2) = deg2rad(10) + deg2rad(30)*sin(2*pi*0.1*time);  % -20 deg to 40 deg
+% 
+% q_ref_ts = timeseries(q,time);
+% 
+% disp("loaded animation constant to feed into MPC");
 
-time = (0:params.Ts:10)';
 
-q = repmat(deg2rad([15 10 5]),length(time),1);
+%% Time
+time = (0:params.Ts:20)';
 
-% q(:,2) = deg2rad(10) + deg2rad(5)*sin(2*pi*0.2*time);  % 5 deg to 10 deg
+roll  = deg2rad(35) * sin(2*pi*0.10*time);
+pitch = deg2rad(30) * sin(2*pi*0.13*time + pi/2);
+yaw   = deg2rad(20) * sin(2*pi*0.07*time + pi/3);
 
-q(:,2) = deg2rad(10) + deg2rad(30)*sin(2*pi*0.1*time);  % -20 deg to 40 deg
+q = [roll pitch yaw];
 
 q_ref_ts = timeseries(q,time);
 
